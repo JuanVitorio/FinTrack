64838437e94c91377df1a7f9bede1e24c70f88d4
Juan Vitório <juanvitorioclash@gmail.com>
2025-04-04 23:44:36 -0300

    feat: Adicionado a parte do front ao projeto

--- backend/fintrack-backend/api/views.py
+++ backend/fintrack-backend/api/views.py
@@ -4,13 +4,13 @@ from transactions.models import Transaction
 from .serializers import TransactionSerializer
 
 @api_view(['GET'])
-def get_data(request): # dados da api
+def get_data(request): # Pegar dados por GET
     item = Transaction.objects.all()
     serializer = TransactionSerializer(item, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def add_data(request):
+def add_data(request): # Enviar dados por post
     serializer = TransactionSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
